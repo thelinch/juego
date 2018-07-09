02e53014943a5da4fa120cc06f1c10b47f74fe2d
thelinch <antony.inga@unas.edu.pe>
2018-07-09 14:15:49 -0500

    total

--- js/index.js
+++ js/index.js
@@ -1,51 +1,74 @@
 var caracteristicas = [{
     nombre: "Adecuacion Funcional",
     identificador: "001",
-    subcaracteristicas: ["Completitud Funcional", "Correccion Funcional", "Pertinencia funcional"],
+    subcaracteristicas: [
+        { nombre: "Completitud Funcional", texto: "Grado en el cual el conjunto de funcionalidades cubre todas las tareas y los objetivos del usuario especificados." },
+        { nombre: "Correccion Funcional", texto: "Capacidad del producto o sistema para proveer resultados correctos con el nivel de precisión requerido." },
+        { nombre: "Pertinencia funcional", texto: "Capacidad del producto software para proporcionar un conjunto apropiado de funciones para tareas y objetivos de usuario especificados." }],
     texto: "proporcionar funciones que satisfacen las necesidades declaradas e implícitas, cuando el producto se usa en las condiciones especificadas"
 }, {
     nombre: "Eficiencia y Desempeño",
     identificador: "002",
-    subcaracteristicas: ["Comportamiento Temporal", "Utilizacion de recursos", "Capacidad"],
+    subcaracteristicas: [
+        { nombre: "Comportamiento Temporal", texto: "Los tiempos de respuesta y procesamiento y los ratios de throughput de un sistema cuando lleva a cabo sus funciones bajo condiciones determinadas en relación con un banco de pruebas (benchmark) establecido." },
+        { nombre: "Utilizacion de recursos", texto: "Las cantidades y tipos de recursos utilizados cuando el software lleva a cabo su función bajo condiciones determinadas." },
+        { nombre: "Capacidad", texto: "Grado en que los límites máximos de un parámetro de un producto o sistema software cumplen con los requisitos." }],
     texto: "representa el desempeño relativo a la cantidad de recursos utilizados bajo determinadas condiciones."
 },
 {
     nombre: "Compatibilidad",
     identificador: "003",
-    subcaracteristicas: ["Coexistencia", "Interoperatibilidad"],
+    subcaracteristicas: [{ nombre: "Coexistencia", texto: "Capacidad del producto para coexistir con otro software independiente, en un entorno común, compartiendo recursos comunes sin detrimento." },
+    { nombre: "Interoperatibilidad", texto: "Capacidad de dos o más sistemas o componentes para intercambiar información y utilizar la información intercambiada." }],
     texto: "capacidad de dos o más sistemas o componentes para intercambiar información y/o llevar a cabo sus funciones requeridas "
 }, {
     nombre: "Usabilidad",
     identificador: "004",
-    subcaracteristicas: ["Reconocer su Adecuación", "Capacidad de Aprendizaje", "Capacidad para ser Usado",
-        "Protección contra errores de usuario", "Estética de la interfaz de usuario", "Accesibilidad"],
+    subcaracteristicas: [
+        { nombre: "Reconocer su Adecuación", texto: "Capacidad del producto que permite al usuario entender si el software es adecuado para sus necesidades." },
+        { nombre: "Capacidad de Aprendizaje", texto: " Capacidad del producto que permite al usuario aprender su aplicaciónw." },
+        { nombre: "Capacidad para ser Usado", texto: "Capacidad del producto que permite al usuario operarlo y controlarlo con facilidad." },
+        { nombre: "Protección contra errores de usuario", texto: "Capacidad del sistema para proteger a los usuarios de hacer errores." },
+        { nombre: "Estética de la interfaz de usuario", texto: "Capacidad de la interfaz de usuario  de agradar y satisfacer la interacción con el usuario." },
+        { nombre: "Accesibilidad", texto: " Capacidad del producto que permite que sea utilizado por usuarios con determinadas características y discapacidades." }],
     texto: "Capacidad del producto software para ser entendido, aprendido, usado y resultar atractivo para el usuario"
 },
 {
     nombre: "Fiabilidad",
     identificador: "005",
-    subcaracteristicas: ["Madurez", "Disponibilidad", "Tolerancia a fallos",
-        "Capacidad de recuperación"],
+    subcaracteristicas: [
+        { nombre: "Madurez", texto: "Capacidad del sistema para satisfacer las necesidades de fiabilidad en condiciones normales." },
+        { nombre: "Disponibilidad", texto: "Capacidad del sistema o componente de estar operativo y accesible para su uso cuando se requiere." },
+        { nombre: "Tolerancia a fallos", texto: "Capacidad del sistema o componente para operar según lo previsto en presencia de fallos hardware o software." },
+        { nombre: "Capacidad de recuperación", texto: "Capacidad del producto software para recuperar los datos directamente afectados y reestablecer el estado deseado del sistema en caso de interrupción o fallo." }],
     texto: "Capacidad de un sistema o componente para desempeñar  las funciones especificadas, cuando se usa bajo unas condiciones y periodo de tiempo determinados"
 },
 {
     nombre: "Seguridad",
     identificador: "006",
-    subcaracteristicas: ["Confidencialidad", "Integridad", "No repudio",
-        "Responsabilidad", "Autenticidad"],
+    subcaracteristicas: [{ nombre: "Confidencialidad", texto: "Capacidad de protección contra el acceso de datos e información no autorizados, ya sea accidental o deliberadamente." },
+    { nombre: "Integridad", texto: " Capacidad del sistema o componente para prevenir accesos o modificaciones no autorizados a datos o programas de ordenador." },
+    { nombre: "No repudio", texto: "Capacidad de demostrar las acciones o eventos que han tenido lugar, de manera que dichas acciones o eventos no puedan ser repudiados posteriormente." },
+    { nombre: "Responsabilidad", texto: "Capacidad de rastrear de forma inequívoca las acciones de una entidad." },
+    { nombre: "Autenticidad", texto: "Capacidad de demostrar la identidad de un sujeto o un recurso." }],
     texto: "Capacidad de protección de la información y los datos de manera que personas o sistemas no autorizados no puedan leerlos o modificarlos"
 },
 {
     nombre: "Mantenibilidad",
     identificador: "007",
-    subcaracteristicas: ["Modularidad", "Reusabilidad", "Analizabilidad",
-        "Capacidad para ser modificado", "Capacidad para ser probado"],
+    subcaracteristicas: [{ nombre: "Modularidad", texto: "Capacidad de un sistema o programa de ordenador (compuesto de componentes discretos) que permite que un cambio en un componente tenga un impacto mínimo en los demás." },
+    { nombre: "Reusabilidad", texto: "Capacidad de un activo que permite que sea utilizado en más de un sistema software o en la construcción de otros activos." },
+    { nombre: "Analizabilidad", texto: "Facilidad con la que se puede evaluar el impacto de un determinado cambio sobre el resto del software, diagnosticar las deficiencias o causas de fallos en el software, o identificar las partes a modificar." },
+    { nombre: "Capacidad para ser modificado", texto: "Capacidad del producto que permite que sea modificado de forma efectiva y eficiente sin introducir defectos o degradar el desempeño." },
+    { nombre: "Capacidad para ser probado", texto: "Facilidad con la que se pueden establecer criterios de prueba para un sistema o componente y con la que se pueden llevar a cabo las pruebas para determinar si se cumplen dichos criterios." }],
     texto: "Esta característica representa la capacidad del producto software para ser modificado efectiva y eficientemente"
 },
 {
     nombre: "Portabilidad",
     identificador: "008",
-    subcaracteristicas: ["Adaptabilidad", "Facilidad de instalacion", "Capacidad de ser reemplazado"],
+    subcaracteristicas: [{ nombre: "Adaptabilidad", texto: "Capacidad del producto que le permite ser adaptado de forma efectiva y eficiente a diferentes entornos determinados de hardware, software, operacionales o de uso." },
+    { nombre: "Facilidad de instalacion", texto: "Facilidad con la que el producto se puede instalar y/o desinstalar de forma exitosa en un determinado entorno." },
+    { nombre: "Capacidad de ser reemplazado", texto: "Capacidad del producto para ser utilizado en lugar de otro producto software determinado con el mismo propósito y en el mismo entorno." }],
     texto: "Esta característica representa la capacidad del producto software para ser modificado efectiva y eficientemente"
 }]
 var template_carta = `<div class="card hoverable subCaracteristica">
@@ -64,7 +87,7 @@ $(document).ready(function () {
     caracteristicas.forEach(element => {
         agregacionTemplateCaracteristicas(element.nombre, "#caracteristicas", colores[Math.round(Math.random() * colores.length - 1)], element.identificador, element)
         element.subcaracteristicas.forEach(subcaracteristicas => {
-            agregacionTemplateSub(subcaracteristicas, "#subCaracteristicas", colores[Math.round(Math.random() * colores.length - 1)], Math.round(Math.random() * 50), element.identificador)
+            agregacionTemplateSub(subcaracteristicas.nombre, "#subCaracteristicas", colores[Math.round(Math.random() * colores.length - 1)], Math.round(Math.random() * 50), element.identificador, subcaracteristicas.texto)
         })
     });
     $(".dragable").draggable({
@@ -78,10 +101,10 @@ $(document).ready(function () {
             comparacion($(this).attr("data-identificador"), $(ui.draggable).attr("data-identificador"), ui.draggable, this)
         }
     });
-    $( ".card" ).tooltip({show: { effect: "explode", duration: 800 }});
+    $(".card").tooltip({ show: { effect: "explode", duration: 500, delay: 700 }, position: { my: "left center", at: "right center" } });
 })
-function agregacionTemplateSub(Nombre, identificado, colorFondo, zindex, identificadorPadre) {
-    let template = `<div class="card hoverable subCaracteristica dragable" data-identificador=${identificadorPadre} style="background:${colorFondo};z-index:${zindex}">
+function agregacionTemplateSub(Nombre, identificado, colorFondo, zindex, identificadorPadre, tooltip) {
+    let template = `<div class="card hoverable subCaracteristica dragable" title='${tooltip}' data-identificador=${identificadorPadre} style="background:${colorFondo};z-index:${zindex}">
 <div class="front">
 <h4 style="color:white"> ${Nombre}</h4> 
 </div>
@@ -94,6 +117,7 @@ function comparacion(caracteristicaNumero, SubCaNumero, elementoDrag, elementoDr
     console.log(caracteristicaNumero, SubCaNumero)
     if (parseInt(caracteristicaNumero) == parseInt(SubCaNumero)) {
         estado = aciertos(elementoDrpp)
+        activarEstrella(elementoDrpp);
     } else {
         estado = fallidos(elementoDrpp)
     }
@@ -104,26 +128,45 @@ function comparacion(caracteristicaNumero, SubCaNumero, elementoDrag, elementoDr
     $(elementoDrag).remove();
 }
 function aciertos(elementoDrop) {
-    $(elementoDrop).find("p#positivo").text( parseInt($(elementoDrop).find("p#positivo").text())+1)
-
+    elemnetoPositivo = $(elementoDrop).find("p.positivo");
+    elemnetoPositivo.text(parseInt(elemnetoPositivo.text()) + 1)
     $("p.acierto strong").text((parseInt($("p.acierto strong").text()) + 1));
     $("i#icon").html("mood")
     return true;
 }
 function fallidos(elementoDrop) {
-    $(elementoDrop).find("p#negativo").text( parseInt($(elementoDrop).find("p#negativo").text())+1)
-        $("p.fallido strong").text((parseInt($("p.fallido strong").text()) + 1))
+    $(elementoDrop).find("p.negativo").text(parseInt($(elementoDrop).find("p.negativo").text()) + 1)
+    $("p.fallido strong").text((parseInt($("p.fallido strong").text()) + 1))
     $("i#icon").html("mood_bad")
     return false;
 }
+function activarEstrella(elementoDrag) {
+    $(elementoDrag).find("i.start").animate({
+        color: "yellow",
+        fontSize: "+=2rem"
+    }, {
+            duration: 500,
+            complete: function () {
+                $(this).animate({ color: "black", fontSize: "2rem" });
+            }
+        })
+}
 function agregacionTemplateCaracteristicas(Nombre, identificador, colorFondo, identificadorPadre, elemento) {
-    
+
     let template = ` <div class="col s12 m3 " style="position:relative" >
     <div class="card small  hoverable caracteristica" title='${elemento.texto}' data-identificador=${identificadorPadre} style="background:${colorFondo}">
     <div class="card-content">
-    <span class="card-title activator grey-text text-darken-4">${Nombre}
-                        <i class="material-icons right">star_border</i>
-                    <div class="divider"></div></span>
+    <div class="row">
+    <div class="col s10">
+    <span class="card-title activator grey-text text-darken-4">${Nombre}</span>
+    </div>
+    <div class="col s2">
+    <i class="material-icons right start small">star_border</i>
+    </div>
+    </div>
+    
+                        
+                    <div class="divider"></div>
                     <div id="re">
                     <ul id="resultados">
     
@@ -137,8 +180,8 @@ function agregacionTemplateCaracteristicas(Nombre, identificador, colorFondo, id
     
 
 <div id="resultado">
-<p class="numero" id="positivo">0</p>
-<p class="numero " id="negativo">0</p>
+<p class="numero positivo" >0</p>
+<p class="numero  negativo" >0</p>
 </div>
 
 
